f22b7395e3fe9ab0f4e48ed4155e0e5cd652dbe6
NatahBish <gameplayer5250@gmail.com>
2019-09-24 18:11:10 -0400

    Update

--- quotable/quotable.console/Program.cs
+++ quotable/quotable.console/Program.cs
@@ -27,21 +27,22 @@ namespace quotable.console
             ///This is where input starts to be needed, when the user puts in "exit" the code will end, else it will return the hard-coded quotes.
             while (!input.Equals("exit"))
             {
-                Console.WriteLine("Please tell me how many quotes you want");
+                Console.WriteLine("Please tell me how many quotes you want, or type 'exit' to end the program");
                 input = Console.ReadLine();
                 long lng = 0;
                 bool isAlong = long.TryParse(input, out lng);
                 if (isAlong)
                 {
                     IEnumerable<string> enumString = simp.getQuote(lng);
-                    
-                    foreach(var str in enumString)
+
+                    foreach (var str in enumString)
                     {
                         Console.WriteLine(str);
                     }
                     Console.WriteLine("");
 
                 }
+                else if (input.Equals("exit")) { }
                 else
                 {
                     Console.WriteLine("Please enter a NUMBER!!!");
